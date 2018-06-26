cf4c7e15a02dfe4b0e6fb10e9ab3e7ae4ef7f93b
Daniel Plisetsky <danplisetsky@gmail.com>
2018-06-26 16:47:36 +0300

    mark current tag with double arrow image

--- assets/javascripts/filter-by-tag.js
+++ assets/javascripts/filter-by-tag.js
@@ -1,23 +1,26 @@
-(function () {
-    const getTag = search => {
-        return decodeURIComponent(
-            search.substr(search.indexOf('?') + 1));    
-    };
+(function() {
+  const getTag = search => {
+    return decodeURIComponent(search.substr(search.indexOf("?") + 1));
+  };
 
-    const getAllPosts = ulClass => {
-        return document.querySelectorAll(`ul.${ulClass}>li`);
-    };
+  const getAllPosts = ulClass => {
+    return document.querySelectorAll(`ul.${ulClass}>li`);
+  };
 
-    const filterPostsByTag = (posts, tag) => {
-        [...posts].forEach(post => {
-            if (!post.dataset.tags.includes(tag))
-                post.remove();    
-        });
-    };
+  const filterPostsByTag = (posts, tag) => {
+    [...posts].forEach(post => {
+      if (!post.dataset.tags.includes(tag)) post.remove();
+    });
+  };
 
-    filterPostsByTag(
-        getAllPosts('post-list'),
-        getTag(document.location.search)
+  const markCurrentTag = tag => {
+    const currentTag = [...document.querySelector(".tags").children].find(
+      el => el.innerText === tag
     );
+    currentTag.classList.add("current-tag");
+  };
 
+  const currentTag = getTag(document.location.search);
+  filterPostsByTag(getAllPosts("post-list"), currentTag);
+  markCurrentTag(currentTag);
 })();
